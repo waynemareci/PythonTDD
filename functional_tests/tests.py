f92e9e88fd2e7f1603fe7cac8c2b1c55e7652934
Wayne Mareci <wayne@mareci.com>
2015-06-28 19:26:42 -0400

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,15 +1,16 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 #import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit()
 
     def check_for_row_in_list_table(self,row_text):
@@ -94,10 +95,10 @@ class NewVisitorTest(LiveServerTestCase):
             
             # She notices the input box is nicely centered
             inputbox = self.browser.find_element_by_id('id_new_item')
-            self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] /2,512,delta=5)
+            self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] /2,512,delta=10)
             
             # She starts a new list and sees the input is nicely
             # centered there too
             inputbox.send_keys('testing\n')
             inputbox = self.browser.find_element_by_id('id_new_item')
-            self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] /2,512,delta=5)
+            self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] /2,512,delta=10)
