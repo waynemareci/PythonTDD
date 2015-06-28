a419cac1628690ea21877c2f8a40dfc472a1c640
Wayne Mareci <wayne@mareci.com>
2015-06-28 15:35:19 -0400

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'superlists.views.home', name='home'),
     url(r'^$', views.home_page, name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url('^lists/the-only-list-in-the-world/$',views.view_list,name='view_list'),
 
     # url(r'^admin/', include(admin.site.urls)),
 ]
