84087e7e8f72e86bf8330753eb29963d69b55265
Wayne Mareci <wayne@mareci.com>
2015-06-28 17:16:12 -0400

    new URL + view for adding to existing lists. FT passes :-)

--- superlists/urls.py
+++ superlists/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     # url(r'^$', 'superlists.views.home', name='home'),
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/new$',views.new_list,name='new_list'),
-    url('^lists/the-only-list-in-the-world/$',views.view_list,name='view_list'),
+    url('^lists/(\d+)/$',views.view_list,name='view_list'),
+    url('^lists/(\d+)/add_item$',views.add_item,name='add_item'),
 
     # url(r'^admin/', include(admin.site.urls)),
 ]
