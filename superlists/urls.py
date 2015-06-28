f31097a01a9bb822bfcd9dc3d1641b50c9126b15
Wayne Mareci <wayne@mareci.com>
2015-06-28 16:04:42 -0400

    changed URLs in forms; moved code in views and tests; new URL

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'superlists.views.home', name='home'),
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/new$',views.new_list,name='new_list'),
     url('^lists/the-only-list-in-the-world/$',views.view_list,name='view_list'),
 
     # url(r'^admin/', include(admin.site.urls)),
