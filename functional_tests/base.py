85d77215891053d7dec680a56ebed5c2be5d5988
Wayne Mareci <wayne@mareci.com>
2015-07-06 06:18:57 -0400

    use wait_for function for URL checks in my_lists

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,14 +1,18 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import WebDriverException
 from datetime import datetime
 import sys
 import os
+import time
 from .server_tools import reset_database
 
+DEFAULT_WAIT = 5
 SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),'screendumps'
 )
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     @classmethod
@@ -32,7 +36,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         if self.against_staging:
             reset_database(self.server_host)
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(DEFAULT_WAIT)
 
     def tearDown(self):
         if self._test_has_failed():
@@ -74,6 +78,16 @@ class FunctionalTest(StaticLiveServerTestCase):
             timestamp=timestamp
         )
 
+    def wait_for(self,function_with_assertion,timeout=DEFAULT_WAIT):
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            try:
+                return function_with_assertion()
+            except(AssertionError,WebDriverException):
+                time.sleep(0.1)
+        # one more try, which will raise any errors if they are outstanding
+        return function_with_assertion()
+        
     def check_for_row_in_list_table(self,row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
